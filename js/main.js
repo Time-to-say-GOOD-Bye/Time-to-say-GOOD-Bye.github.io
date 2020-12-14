e0122904f5c4ac123a5ea016455e2b1a70d254f2
Kiyu <knishida20@iamas.ac.jp>
2020-12-14 13:22:13 +0000

    complete frame

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+imageMapResize();
 $(function(){
     $(".btn-gnavi").on("click", function(){
         // ハンバーガーメニューの位置を設定
@@ -17,3 +18,28 @@ $(function(){
         }, 200);
     });
 });
+
+// // Load the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // Replace the 'ytplayer' element with an <iframe> and
+// // YouTube player after the API code downloads.
+// var player;
+// function onYouTubePlayerAPIReady() {
+//   player = new YT.Player('video_wrapper', {
+//     height: '360',
+//     width: '640',
+//     videoId: 'Q9oUQYsApxg',
+//     events: {
+//         'onReady': onPlayerReady,
+//       }
+//   });
+// }
+
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+//     console.log("aa")
+//   }
